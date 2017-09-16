5b9dcf85ceb84cdadc83a4c4fb7752c25107e215
Kenny Chen <kc5by@virginia.edu>
2017-09-16 14:41:37 -0400

    Bullets
    Bullet Firing

---
+++ Dusk Patrol/Assets/resources/Scripts/BasicBullet.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicBullet : MonoBehaviour
+{
+    public float damage;
+    private float speed = 2;
+    private Rigidbody2D rigidBody;
+
+	void Awake ()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();	
+	}
+	
+	void Update ()
+    {
+        MoveBullet();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        Destroy(gameObject);
+    }
+
+    void MoveBullet()
+    {
+        transform.position += new Vector3(0, speed * Time.deltaTime, 0);
+    }
+}
