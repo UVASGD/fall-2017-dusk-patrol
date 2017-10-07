65b59821f8b03c41033458230cbae9facbb0ec98
Ersake <ersake@gmail.com>
2017-10-07 12:45:43 -0400

    Enemy Movement
    Basic enemy movement script, override the getMovement method in order to create new paths.

---
+++ Dusk Patrol/Assets/resources/Scripts/EnemyMovement.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour {
+
+    private float speed = 10;
+    private Rigidbody2D myRigidBody;
+    private Vector2 movement;
+    private float t;
+    // Use this for initialization
+    private void Awake()
+    {
+        myRigidBody = GetComponent<Rigidbody2D>();
+        movement = new Vector2(0, 0);
+        t = 0;
+    }
+    	
+	// Update is called once per frame
+	void Update () {
+        t += Time.deltaTime;
+        movement = this.getMovement(t);
+        gameObject.GetComponent<Rigidbody2D>().velocity = movement*speed*Time.deltaTime;
+     }
+
+    Vector2 getMovement(float t)
+    {
+        return new Vector2(1, -2 * t);
+    }
+}
