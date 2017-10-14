7e83e247ac2ec3d900c2be31d2cd0708b9fcaff3
jwt2en <jwt2en@virginia.edu>
2017-10-14 13:03:54 -0400

    Bullet Time Travel
    Start of bullet traveling back in time.

---
+++ Dusk Patrol/Assets/resources/Scripts/BulletTimeTravel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletTimeTravel : MonoBehaviour {
+    
+    // Use this for initialization
+    void Start () {
+        float pos = transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float timefactor = FindObjectOfType<TimeManager>().timeFactor;
+        if (timefactor == -1) // Object is moving back in time
+        {
+            
+        }
+	}
+}
