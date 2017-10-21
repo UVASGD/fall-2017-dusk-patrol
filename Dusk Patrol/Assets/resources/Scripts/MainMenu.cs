3951ba85b72aa683c3b6e97ebf56158b66cd78f7
Kenny Chen <kc5by@virginia.edu>
2017-10-20 21:39:36 -0400

    Added Main Menu

---
+++ Dusk Patrol/Assets/resources/Scripts/MainMenu.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenu : MonoBehaviour
+{
+    Button startButton;
+
+    void Awake()
+    {
+        startButton = (Button)GameObject.Find("StartButton").GetComponent<Button>();
+        startButton.onClick.AddListener(() => OnClick());
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnClick()
+    {
+        SceneManager.LoadScene("Main");
+    }
+}
