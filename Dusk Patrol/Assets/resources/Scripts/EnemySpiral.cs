9b68c7dedd685fc5f2c6c47187f2a670602db182
Ersake <ersake@gmail.com>
2017-10-20 21:11:48 -0400

    Movement Paths
    Made 4 movement paths with associated prefab enemies.

---
+++ Dusk Patrol/Assets/resources/Scripts/EnemySpiral.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySpiral : EnemyMovement
+{
+    public float size;
+    public override Vector2 getMovement(float t)
+    {
+        float x = speed*(size *Mathf.Sin(t));
+
+        float y = (-speed * Mathf.Sin(t + (size - 1) * Mathf.PI / (size * 2)) - 120);
+
+        return new Vector2(x, y);
+    }
+
+
+}
+
---
+++ Dusk Patrol/Assets/resources/Scripts/EnemyZigZag.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyZigZag : EnemyMovement {
+
+    public float direction;
+
+    public override Vector2 getMovement(float t)
+    {
+        float x = -1000*direction;
+        float y=0;
+        if (t<1)
+        {
+            y = 0;
+        }
+        else if(t>=1 && t<3){
+            y = -1000;
+        }
+        else if(t>=3 && t<5){
+            y = 1000;
+        }
+        else if(t>=5 && t<7)
+        {
+            y = -1000;
+        }
+        else if (t>=7)
+        {
+            y = 0;
+        }
+
+        //Debug.Log(" y " + y + " time " + t);
+        return new Vector2(y, x);
+    }
+}
