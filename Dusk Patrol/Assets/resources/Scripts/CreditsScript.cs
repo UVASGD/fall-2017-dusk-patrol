327e7aa8354f66d1b40c205301965605a80d0500
Jack Herd <jherd23@gmail.com>
2017-10-22 00:48:04 -0400

    Allows the User to return from credit Scene

---
+++ Dusk Patrol/Assets/resources/Scripts/CreditsScript.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class CreditsScript : MonoBehaviour {
+
+	Button back;
+
+	// Use this for initialization
+	void Start () {
+		back = (Button)GameObject.Find ("BackButton").GetComponent<Button> ();
+		back.onClick.AddListener (() => OnClick ());
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnClick() {
+		SceneManager.LoadScene (0);
+	}
+}
