d029a7eebb5559bbc77947b35ae4dc95f9562b4a
Jack Herd <jherd23@gmail.com>
2017-11-12 12:23:30 -0500

    Background Music Added

---
+++ Dusk Patrol/Assets/resources/Scripts/AudioPlayer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour {
+
+	private AudioSource a;
+
+	// Use this for initialization
+	void Start () {
+		a = GetComponent<AudioSource> ();
+		a.loop = true;
+		a.Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
