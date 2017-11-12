7675d0cac1e09111dcb13407ead61cee81f286d7
Ersake <ersake@gmail.com>
2017-11-12 13:56:09 -0500

    Boss Laser on enemy sprite
    Got the laser to work

---
+++ Dusk Patrol/Assets/resources/Scripts/BossLaserShooting.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossLaserShooting : MonoBehaviour {
+
+    public GameObject bossLaser;
+        private HealthScript health;
+    private float maxCoolDown = 1f;
+    private float currCoolDown = 0f;
+    private float[] shootTimes;
+
+    private float timer = 0f;
+    private int shootTimesIndex = 0;
+
+    private void Awake()
+    {
+        health = GetComponent < HealthScript >();
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (!health.isDead)
+        {
+            if (TimeManager.timeFactor > 0)
+            {
+                Vector3 viewPos = Camera.main.WorldToViewportPoint(gameObject.transform.position);
+                if (viewPos.y >= 0 && viewPos.y <= 1)
+                {
+                    timer += Time.deltaTime * TimeManager.timeFactor;
+                    if (timer % 10f <=2)
+                    {
+                        Shoot(bossLaser);
+                    
+                    }
+                    else
+                    {
+                        Despawn(bossLaser);
+                    }
+                }
+            }
+        }
+    }
+    void Shoot(GameObject bullet)
+    {
+        Instantiate(bossLaser, transform.position - Vector3.up, Quaternion.identity);
+    }
+    void Despawn(GameObject bullet)
+    {
+        Destroy(bullet);
+    }
+}
