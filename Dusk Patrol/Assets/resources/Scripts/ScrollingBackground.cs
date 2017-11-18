cb49b78c99075d0366a3c1b7f5f7f0d95e54f6c0
ArtixBot <williamwongcastle@gmail.com>
2017-11-18 13:27:48 -0500

    SCROLLING BACKGROUND woo
    N E A T
    E
    A
    T

---
+++ Dusk Patrol/Assets/resources/Scripts/ScrollingBackground.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ScrollingBackground : MonoBehaviour
+{
+	public float scrollSpeed;
+	public float tileSizeY;
+
+	private Vector2 startPosition;
+
+	void Start ()
+	{
+		startPosition = transform.position;
+	}
+
+	void Update ()
+	{
+		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);
+		Vector3 newpos = transform.position = startPosition + Vector2.up * newPosition;
+		newpos.z += 60.0f;
+		transform.position = newpos;
+	}
+}
