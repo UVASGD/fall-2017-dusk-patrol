43ff93f1f6db47c060c052be77f702392a6a60bc
ArtixBot <williamwongcastle@gmail.com>
2017-11-18 13:29:39 -0500

    comments added to scrolling background script because it turns out copying and pasting's bad for documentation

--- Dusk Patrol/Assets/resources/Scripts/ScrollingBackground.cs
+++ Dusk Patrol/Assets/resources/Scripts/ScrollingBackground.cs
@@ -17,6 +17,8 @@ public class ScrollingBackground : MonoBehaviour
 	void Update ()
 	{
 		float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSizeY);
+
+		// it has to be this way. why? because c# is being annoying.
 		Vector3 newpos = transform.position = startPosition + Vector2.up * newPosition;
 		newpos.z += 60.0f;
 		transform.position = newpos;
