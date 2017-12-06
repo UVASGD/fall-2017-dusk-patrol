09f7a057a49998597ad0a87a1b0932bd53d35e4a
ArtixBot <williamwongcastle@gmail.com>
2017-12-05 20:41:00 -0500

    My god, the score persists along scenes!

---
+++ Dusk Patrol/Assets/resources/Scripts/DethScoreScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class DethScoreScript : MonoBehaviour {
+
+	public float dethScore;
+	public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		dethScore = ScoreScript.score;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		scoreText.text = ((int) dethScore).ToString();
+	}
+}
--- Dusk Patrol/Assets/resources/Scripts/ScoreScript.cs
+++ Dusk Patrol/Assets/resources/Scripts/ScoreScript.cs
@@ -4,18 +4,21 @@ using System.Collections;
 
 public class ScoreScript : MonoBehaviour {
 
-	float score;
+	public static float score;
+	public float dethScore;
 	public Text scoreText;
 
 	// Use this for initialization
 	void Start () {
 		score = 0;
+		dethScore = 0;
 		scoreText.text = score.ToString();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		score += (Time.deltaTime * TimeManager.timeFactor);
+		dethScore = score;
 		if (score < 0) {
 			score = 0;
 		}
