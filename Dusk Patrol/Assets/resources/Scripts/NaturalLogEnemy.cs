77ac128a52e58c0889b92ae86b8fae9d5b57fe68
ArtixBot <williamwongcastle@gmail.com>
2017-12-05 19:30:45 -0500

    Fixes enemy prefabs and begins work on actual level 1

--- Dusk Patrol/Assets/resources/Scripts/NaturalLogEnemy.cs
+++ Dusk Patrol/Assets/resources/Scripts/NaturalLogEnemy.cs
@@ -7,7 +7,7 @@ public class NaturalLogEnemy : EnemyMovement {
 	public float direction;
 
 	public override Vector2 getMovement(float t) {
-		float x = direction * Mathf.Exp(t) / (100*t);
+		float x = direction * Mathf.Exp(Mathf.Min(t,10)) / (100*t);
 		float y = -1;
 		return new Vector2 (x, y);
 	}
