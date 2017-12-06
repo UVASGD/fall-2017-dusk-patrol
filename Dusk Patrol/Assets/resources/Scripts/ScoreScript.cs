a49e76f40341105af451bfe12e84d8f31857f684
ArtixBot <williamwongcastle@gmail.com>
2017-12-05 19:58:50 -0500

    shit
    please work

---
+++ Dusk Patrol/Assets/resources/Scripts/ScoreScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreScript : MonoBehaviour {
+
+	int score;
+	public Text scoreText;
+
+	// Use this for initialization
+	void Start () {
+		score = 0;
+		scoreText.text = score.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		score++;
+		scoreText.text = score.ToString ();
+	}
+}
